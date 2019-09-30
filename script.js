73f5cf9dd8d4eb0f3d6734b4483d7625a95eb411
cis101 <cis101@secs.iub.edu.bd>
2019-09-30 09:24:32 +0600

    Task 2

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 /* JavaScript code here */
 alert("HACKED!!!")
+//Task 2
+function arraySum(arr){
+    let sum=0
+  arr.forEach(x =>{
+sum=sum+x
+  })
+  return sum
+}
+    let sum=arraySum([1,2,3])
+    console.log(sum)
