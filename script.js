78299118cb4cefcfdb33430d6bbf2f0f47620519
Sabah97 <sabah.ashraf97@gmail.com>
2019-10-02 08:44:47 +0600

    Task 3

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ sum=sum+x
 }
     let sum=arraySum([1,2,3])
     console.log(sum(5))
+
+
+    // task 3
+
+    function validEmail(email){
+      var regex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+     return regex.test(email);
+
+    }
+    console.log(validEmail("cffhdf"));
